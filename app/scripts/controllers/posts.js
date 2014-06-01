a3f292f15123112f09bd8d2ef312d21ab083dbde
Jan Milosh <janmilosh@gmail.com>
2014-06-01 07:12:19 -0400

    Adds ng-include to create a navbar & filters to format data

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -3,14 +3,7 @@
 app.controller('PostsController', function($scope, $location, Post) {
 
   $scope.posts = Post.all;
-  $scope.post = {url: 'http://', 'title': ''};
   
-  $scope.submitPost = function() {
-    Post.create($scope.post).then(function(ref) {
-      $location.path('/posts/' + ref.name());
-    });
-  };
-
   $scope.deletePost = function(postId) {
     Post.delete(postId);
   };
